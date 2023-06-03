29ebf3e1bc015c24c09314a1f8fbe6b2328cbbeb
george <georgij5ptb@gmail.com>
2023-06-03 12:05:50 +0300

    3/class/1

---
+++ Lesson_3/Class_work/Task_1/I_MyNum.java
@@ -0,0 +1,6 @@
+package Lesson_3.Class_work.Task_1;
+
+public interface I_MyNum {
+    double getNum(double x);
+
+}
---
+++ Lesson_3/Class_work/Task_1/main.java
@@ -0,0 +1,15 @@
+package Lesson_3.Class_work.Task_1;
+
+public class main {
+    public static void main(String[] args) {
+        I_MyNum myNum_1;//создать ссылку на экземпляр
+
+        myNum_1=(y)->98.6/y;//реализация
+
+        System.out.println(myNum_1.getNum(12.4));
+
+        System.out.println(myNum_1.getNum(7.951612903225806));
+
+
+    }
+}
