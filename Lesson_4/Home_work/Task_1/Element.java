d081bb90715f469f5ae3f2d729b92e52516c7005
george <georgij5ptb@gmail.com>
2023-06-18 00:11:41 +0300

    4/home/1

--- Lesson_4/Home_work/Task_1/Element.java
+++ Lesson_4/Home_work/Task_1/Element.java
@@ -1,11 +1,13 @@
 package Lesson_4.Home_work.Task_1;
 
-public class Element <T>{
+public class Element<T> {
     T t;
-    Element(T t ){
-        this.t=t;
+
+    Element(T t) {
+        this.t = t;
     }
-    public void printElement(){
+
+    public void printElement() {
         System.out.println(t);
         System.out.println(t.getClass());
     }
--- Lesson_4/Home_work/Task_1/main.java
+++ Lesson_4/Home_work/Task_1/main.java
@@ -1,11 +1,12 @@
 package Lesson_4.Home_work.Task_1;
+
 import Lesson_4.Home_work.Task_1.Element;
 
 public class main {
     public static void main(String[] args) {
-        Element<Integer> e_1=new Element<Integer>(1);
-        Element<String> e_2=new Element<String>("text");
-        Element<Double> e_3=new Element<Double>(12.3);
+        Element<Integer> e_1 = new Element<Integer>(1);
+        Element<String> e_2 = new Element<String>("text");
+        Element<Double> e_3 = new Element<Double>(12.3);
 
         e_1.printElement();
         e_2.printElement();
